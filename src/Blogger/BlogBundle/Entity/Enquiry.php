2960d29e65194a53ea46bbb3740e0769d025370c
Andrii Zahura <shkiper@2261152.no-reply.drupal.org>
2017-02-08 08:05:09 +0200

    add form validation

--- src/Blogger/BlogBundle/Entity/Enquiry.php
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -2,6 +2,11 @@
 
 namespace Blogger\BlogBundle\Entity;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 class Enquiry
 {
@@ -68,6 +73,20 @@ class Enquiry
    */
   public function setBody($body) {
     $this->body = $body;
+  }
+
+  public static function loadValidatorMetadata(ClassMetadata $metadata)
+  {
+    $metadata->addPropertyConstraint('name', new NotBlank());
+    $metadata->addPropertyConstraint('email', new Email(array(
+      'message' => 'Symblog does not like invalid emails. Give me a real one!'
+    )));
+    $metadata->addPropertyConstraint('subject',  new Length(array(
+      'max'        => 50
+    )));
+    $metadata->addPropertyConstraint('body', new Length(array(
+      'min'        => 50
+    )));
   }
 
 }
